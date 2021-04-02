82a861ca86022cfd489083e2676e4d200f7e7594
Sandeep <sxk80270@ucmo.edu>
2021-04-02 00:07:58 -0400

    Adding React Burger App Code

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
+
+ class BurgerBuilder extends Component {
+     state = {
+         ingredients: {
+            salad: 3,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+         }
+     }
+     render () {
+         return(
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <div>Burger Builder</div>
+            </Aux>
+         );
+     }
+ }
+
+export default BurgerBuilder;
